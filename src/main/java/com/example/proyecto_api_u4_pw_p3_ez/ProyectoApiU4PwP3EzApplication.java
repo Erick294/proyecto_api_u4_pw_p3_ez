626a92c6c7016f016cf4cadf47db32791e2a143f
Erick294 <beowulfblackman@hotmail.com>
2023-02-22 19:46:36 -0500

    Taller 33

---
+++ src/main/java/com/example/proyecto_api_u4_pw_p3_ez/ProyectoApiU4PwP3EzApplication.java
@@ -0,0 +1,13 @@
+package com.example.proyecto_api_u4_pw_p3_ez;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoApiU4PwP3EzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoApiU4PwP3EzApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/proyecto_api_u4_pw_p3_ez/ProyectoApiU4PwP3EzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.proyecto_api_u4_pw_p3_ez;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoApiU4PwP3EzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
